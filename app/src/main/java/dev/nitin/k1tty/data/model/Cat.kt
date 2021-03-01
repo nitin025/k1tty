cb1724241bb8c23871ef8c3067ec657c77985733
Nitin Sabale <nitinsabale59@gmail.com>
2021-03-02 00:34:59 +0530

    Data Model added

---
+++ app/src/main/java/dev/nitin/k1tty/data/model/Cat.kt
@@ -0,0 +1,15 @@
+package dev.nitin.k1tty.data.model
+
+data class Cat(
+    val id: Int,
+    val name: String,
+    val age: Double,
+    val gender: String,
+    val color: String,
+    val weight: Double,
+    val location: String,
+    val image: Int,
+    val story: String,
+    val user: User
+
+)
---
+++ app/src/main/java/dev/nitin/k1tty/data/model/User.kt
@@ -0,0 +1,7 @@
+package dev.nitin.k1tty.data.model
+
+data class User(
+    val id: Int,
+    val name: String,
+    val image: Int
+)
